b7a37cb3170da702ef6a7c7aaa2d67f5ea381f6a
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2017-12-08 09:00:41 +0100

    init

---
+++ LOGIC_Fsharp/Logic_Fsharp.fs
@@ -0,0 +1,4 @@
+﻿namespace LOGIC_Fsharp
+
+type Class1() = 
+    member this.X = "F#"
---
+++ f_sharp_vs_c_sharp/Form.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace f_sharp_vs_c_sharp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_cLogic = new System.Windows.Forms.Label();
+            this.lbl_fLogic = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_cLogic
+            // 
+            this.lbl_cLogic.AutoSize = true;
+            this.lbl_cLogic.Location = new System.Drawing.Point(49, 13);
+            this.lbl_cLogic.Name = "lbl_cLogic";
+            this.lbl_cLogic.Size = new System.Drawing.Size(46, 17);
+            this.lbl_cLogic.TabIndex = 0;
+            this.lbl_cLogic.Text = "label1";
+            // 
+            // lbl_fLogic
+            // 
+            this.lbl_fLogic.AutoSize = true;
+            this.lbl_fLogic.Location = new System.Drawing.Point(52, 54);
+            this.lbl_fLogic.Name = "lbl_fLogic";
+            this.lbl_fLogic.Size = new System.Drawing.Size(46, 17);
+            this.lbl_fLogic.TabIndex = 1;
+            this.lbl_fLogic.Text = "label2";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 658);
+            this.Controls.Add(this.lbl_fLogic);
+            this.Controls.Add(this.lbl_cLogic);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_cLogic;
+        private System.Windows.Forms.Label lbl_fLogic;
+    }
+}
+
