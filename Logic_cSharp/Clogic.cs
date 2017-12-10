f2694fda03ea5b002fcb49dffe31fbf62dce3999
rasmniel <rasmniel@gmail.com>
2017-12-10 14:33:11 +0100

    CS logic updated with functionality

--- Logic_cSharp/Clogic.cs
+++ Logic_cSharp/Clogic.cs
@@ -1,8 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Logic_cSharp
 {
     public class Clogic
     {
+        public List<int> sortList(List<int> list)
+        {
+            Quicksort(list.ToArray(), 0, list.Count);
+            return list;
+        }
+
+        private void Quicksort(int[] list, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = list[(left + right) / 2];
+            while (i <= j)
+            {
+                while (list[i].CompareTo(pivot) < 0)
+                    i++;
+                while (list[j].CompareTo(pivot) > 0)
+                    j--;
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = list[i];
+                    list[i] = list[j];
+                    list[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+                Quicksort(list, left, j);
+            if (i < right)
+                Quicksort(list, i, right);
+        }
+
+        public List<int> reverseList(List<int> list)
+        {
+            int[] array = new int[list.Count];
+            int i = array.Length;
+            foreach (int val in list)
+                array[i--] = val;
+
+            return new List<int>(array);
+        }
+
+        public int calculate(List<int> list)
+        {
+            int result = 0;
+            foreach (int i in list)
+                result += i;
+
+            return result;
+        }
+
     }
 }
